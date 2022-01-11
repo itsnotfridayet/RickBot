e26c14219cba8eb886369e525b0358d77a5619c4
itsnotfridayet <59158911+itsnotfridayet@users.noreply.github.com>
2022-01-11 20:36:50 +0000

    added better slash command support.

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,8 @@ const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
 
